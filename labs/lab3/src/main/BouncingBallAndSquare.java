9a831d3f411f8b77a0fec07963ae593bcd717b33
Cody Martin <cody@Codys-MBP.fios-router.home>
2018-05-04 23:32:08 -0400

    sample stuff in there now

--- labs/lab3/src/main/BouncingBallAndSquare.java
+++ labs/lab3/src/main/BouncingBallAndSquare.java
@@ -4,12 +4,14 @@ import java.awt.*;
 
 public class BouncingBallAndSquare extends JComponentWithEvents {
 
-    public void timerFired() {
+    private int rectX = 0;
 
+    public void timerFired() {
+        rectX += 1;
     }
 
     public void paint(Graphics2D page) {
-
+        page.fillRect(rectX, 0, 10, 10);
     }
     // Standard main method:
     public static void main(String[] args) { launch(500, 400); }
